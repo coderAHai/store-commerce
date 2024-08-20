a4362039dc29e9d19cca06018e4c22cb53071007
coderAHai <coderyue@qq.com>
2024-08-20 20:41:56 +0800

    修改网页head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   srcDir: './src',
+  app: {
+    head: {
+      title: '.Store',
+      meta: [
+        { name: 'description', content: 'store commerce' },
+        { name: 'keywords', content: 'commerce' }
+      ],
+      link: [{ rel: 'icon', href: 'favicon.ico' }]
+    }
+  },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', '@pinia/nuxt', 'nuxt-swiper'],
   css: ['~/assets/styles/common.css', 'swiper/css', 'swiper/css/navigation', 'swiper/css/pagination'],
   icon: {
